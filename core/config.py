f94a4972c83b90cc65dc46f41ed1b9af4b4e3809
FidelCoder7 <fidelokemwa211@gmail.com>
2026-06-10 11:24:59 +0300

    fix:resolve workflow errors

--- core/config.py
+++ core/config.py
@@ -5,9 +5,15 @@ load_dotenv()
 
 
 class Settings:
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    SECRET_KEY = os.getenv(
+        "SECRET_KEY",
+        "test-secret-key-for-ci"
+    )
 
-    ALGORITHM = os.getenv("ALGORITHM")
+    ALGORITHM = os.getenv(
+        "ALGORITHM",
+        "HS256"
+    )
 
     ACCESS_TOKEN_EXPIRE_MINUTES = int(
         os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)
--- database/database.py
+++ database/database.py
@@ -6,11 +6,16 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///job_application_tracker.db"
+    )
 
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
+    if DATABASE_URL.startswith("sqlite")
+    else{}
 )
 
 SessionLocal = sessionmaker(
